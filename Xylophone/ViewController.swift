a279460551e47fc044502558b9bb3c8d39e56960
Greg Swan <gmswan99@gmail.com>
2023-09-11 11:39:27 -0400

    Update URL resource

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     
     
     func playSound(soundName: String) {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
