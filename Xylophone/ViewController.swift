4c149c27452bb49ba9909f86c01813a708960afb
Greg Swan <gmswan99@gmail.com>
2023-09-10 20:00:05 -0400

    Add parameter soundName

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print(sender.currentTitle ?? <#default value#>)
+        
+        playSound(soundName: <#String#>)
+    }
     
-
+    
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
